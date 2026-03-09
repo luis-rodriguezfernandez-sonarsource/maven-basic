b0b85c01d7d76eece76cb81205024b65738252d5
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2026-03-09 17:06:40 +0100

    add some random and faulty logic to bar

--- src/main/java/com/acme/basic/Bar.java
+++ src/main/java/com/acme/basic/Bar.java
@@ -2,7 +2,35 @@ package com.acme.basic;
 
 public class Bar {
 
+    int matrix1[][];   // Noncompliant
+    int[] matrix2[];   // Noncompliant
+
     public void barMethod() {
         System.out.println("Bar");
+
+        System.out.println("test-inline");
+    }
+
+    public void sumMatrices() {
+        int sum = 0;
+        if (matrix1 != null) {
+            for (int[] row : matrix1) {
+                if (row != null) {
+                    for (int val : row) {
+                        sum += val;
+                    }
+                }
+            }
+        }
+        if (matrix2 != null) {
+            for (int[] row : matrix2) {
+                if (row != null) {
+                    for (int val : row) {
+                        sum += val;
+                    }
+                }
+            }
+        }
+        System.out.println("Sum of all elements: " + sum);
     }
 }
