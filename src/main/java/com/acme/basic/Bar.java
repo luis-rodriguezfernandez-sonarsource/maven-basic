4746499a9d67419d7f276301b4d33562050beb93
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2026-03-20 11:54:26 +0100

    java:S2053

--- src/main/java/com/acme/basic/Bar.java
+++ src/main/java/com/acme/basic/Bar.java
@@ -1,8 +1,15 @@
 package com.acme.basic;
 
+import javax.crypto.spec.PBEParameterSpec;
+
 public class Bar {
 
     public void barMethod() {
         System.out.println("Bar");
+    }
+
+    public void hash() {
+        byte[] salt = "salty".getBytes();
+        PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Noncompliant
     }
 }
